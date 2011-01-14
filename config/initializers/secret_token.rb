b8e6a1b3ca335bc09a4ce3399d3bad2c685f61c2
livesessions <livesessions@livesessions-laptop.(none)>
2011-01-13 19:25:24 -0600

    First commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+RailsSite::Application.config.secret_token = 'ba8b51980a80a51df36357cff86c17bfa9aaa1ce41d5cea3ccc16ba5571a7bbc43af473c80197cd8fa8cccfafafa5b4701df42f5459669d6c6747b1c5520870e'
